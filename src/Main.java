881035c88729e52efaae70808fd66aa690334993
tetnik <t.a.t.i.a.n.a.0.1.n.i.k.o@gmail.com>
2025-06-19 20:37:09 +0200

    Added initial game setup with intro,health/luck system, and adventure events

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // НИЖЧЕ ПРИКЛАД КОДУ, ВИКОРИСТОВУЙТЕ ЙОГО ЯК ЗРАЗОК,
-        // АЛЕ САМ ПРИКЛАД МОЖНА ВИДАЛИТИ ПОТІМ
-        // Приклад сканнера для перехоплення вводу користувача з консолі
-        System.out.println("Привіт, відважний герою!");
-        System.out.println("Тебе чекає цікава подорож");
-        System.out.println("Ти йшов чарівною стежкою, і натрапив на роздоріжжя: 1 - піти наліво, 2 - піти направо");
+        Scanner scanner = new Scanner(System.in);
+        int health = 100;
+        int luck = 50;
+        System.out.println("Welcome to the fairy forest!Many adventures await ahead!");
+        System.out.println("Your task is to go through the forest, making the right decisions.");
+        System.out.println("Your current Health: " + health + ", Luck: " + luck );
+        System.out.println("You have found a crossroad.Choose a path : 1 - Old shaky path , 2 - Treasure among the bushes");
         Scanner sc = new Scanner(System.in);
-        // Таким чином можна отримати цифру яку ввів користувач
-        // і зберегти її в змінну типу int
-        int choice1 = sc.nextInt();
+        int choice1 = 2;
         if (choice1 == 1) {
-            System.out.println("Ти обрав піти наліво");
-            // відняти очки здоров*я
+            System.out.println("You took the old shaky path. You twisted your ankle. Health decreased by 35.");
+            health -=35;
         } else if (choice1 == 2) {
-            System.out.println("Ти обрав піти направо");
-            // додати очки удачі
+            System.out.println("You have found a treasure among the bushes! Luck increased by 25 points.");
+            luck +=25;
         } else {
-            System.out.println("Ти заплутався");
-            // відняти очки здоров*я
+            System.out.println("You stand still, unsure what to do. It's already dark, you are starving. Health -15.");
+            health -=15;
         }
-        // КІНЕЦЬ ПРИКЛАДУ, ВИДАЛІТЬ ПРИКЛАД, ЩОБ В КОДІ БУЛО ТІЛЬКИ ВАШЕ РІШЕННЯ
+         System.out.println("Here is a mysterious well. What will you do? 1 - Drink from the well , 2 - Ignore");
+        int choice2 = 1;
+        if (choice2 == 1) {
+            System.out.println("You decided to drink from the well... Magic water has restored you to 30 health points.");
+            health +=30;
+        } else if (choice2 == 2) {
+            System.out.println("Keep going, there's a lot of interesting things ahead!");
+        } else {
+            System.out.println("You stand still, unsure what to do. It's already dark, you are starving. Health -5.");
+            health -=5;
+        }
+        System.out.println("You have met a magical creature. What will happen next?The creature was not friendly! You had a fight with it. -50 from health.");
+        health -=50;
+        System.out.println("Final score");
+        System.out.println("Health: " + health);
+        System.out.println("Luck: " + luck);
 
-
-
-        // Нижче пишіть ваш код з реалізацією завдання
-        // Коли пишете логіку гри - не забувайте періодично запускати програму
-        // І переконуватись, що все працює як треба, заодно тестуйте її перебираючи інпути
-
+        scanner.close();
     }
 
 }
